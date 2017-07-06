6817aeb20a5b5690da98e204e4c3133137c7e7c5
liuding <liuding@ifmuse.com>
2017-07-06 18:55:48 +0800

    add laravel middleware

---
+++ laravel/middleware.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuding
+ * Date: 2017/7/6
+ * Time: 下午4:52
+ */
+
+
+/**
+
+中间件
+
+中间件是在路由和路由对应的控制器之间，用于对请求进行合理性检测。
+常用作用：
+用于用户登录状态检测
+用于用户权限检测
+用于请求日志的记录
+ 
+1、创建中间件
+
+通过命令行创建，需要进入laravel根目录，执行：
+php artisan make:middleware YourMiddleware
+
+这个创建好的中间件，将被存放于：laravel根路径下：app/Http/Middleware下
+
+2、修改中间件代码：
+
+3、注册中间件
+
+全局注册 ： 所有路由都要限制
+路由注册 ： 对某个路由或者路由组进行限制
+
+注册方法：打开laravel根路径下的App/Http/Kernal.php文件，
+变量$middleware=[] , 表示注册全局中间件；
+变量$routeMiddleware=[] ,表示注册路由中间件
+
+4、全局注册的应用 -- TestMiddleware
+
+1> 创建
+php artisan make:middleware TestMiddleware
+
+2> 修改
+代码1
+
+3> 注册：
+//::class表示类的带有命名空间的全路径
+protected $middleware = [
+	\App\Http\Middleware\TestMiddleware::class
+]
+
+修改配置文件:
+//::class表示类的带有命名空间的全路径
+protected $middleware = [
+	\App\Http\Middleware\TestMiddleware::class
+]
+
+
+5、路由注册的应用 -- LoginMiddleware
+
+1> 创建
+php artisan make:middleware LoginMiddleware
+
+2> 修改
+代码2
+
+3> 注册：
+protected $middleware = [
+	"login" => \App\Http\Middleware\LoginMiddleware::class
+]
+
+4> 应用
+// 单个路由限制 -- 方式一
+Route::get("/info" , [
+	"middleware" => "login" ,
+	"uses" => function(){
+		echo "这是info页面" ;
+	}
+]) ;
+// 单个路由限制 -- 方式二
+Route::get("/admin" , function(){
+	echo "这是后台页面！" ;
+}).middleware("login");
+
+// 设置session
+Route::get("/session" , function(){
+	return session(["uid" => 10]);
+});
+
+*/
+
+// 代码1
+<?php
+	
+	namespace App\Http\Middleware ;
+
+	use Closure ;
+
+	class TestMiddleware {
+		
+		/**
+		* Handle an incoming request
+		*
+		* @param \Illuminate\Http\Request $request
+		* @param \Closure $next
+		* @param mixed
+		*/
+		public function handle($request , Closure $next){
+			// 记录该请求的路径
+			// $request 表示请求相关的信息
+			$path = '[' . date('Y:m:d H:i:s')  . ']' . $request->ip() . '------' . $request->path() ;
+			// 将请求路径存入request.log日志文件中，此时request.log是相对于请求的文件的路径而生成的
+			// FILE_APPEND -- 表示追加的方式
+			file_put_contents('request.log' , $path."\r" , FILE_APPEND) ;
+			// $next 是变量函数 ，表示下一个中间件
+			return $next($request) ;
+		}
+	}
+
+?>
+
+
+<?php
+	
+	namespace App\Http\Middleware ;
+	use Closure ;
+
+	class LoginMiddleware {
+		/**
+		* Handle an incoming request
+		*
+		* @param \Illuminate\Http\Request $request
+		* @param \Closure $next
+		* @param mixed
+		*/
+		public function handle($request , Closure $next){
+
+			// 判断session是否存在
+			if(!session("uid")){
+				// 跳转
+				redirect("/login") ;
+			}
+			return $next($request);
+		}
+	}
+?>
+
+
+
+
+ 
+
+
---
+++ laravel/route.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuding
+ * Date: 2017/7/6
+ * Time: 下午3:33
+ */
+
+/**
+laravel 框架
+1、框架的路由文件放在：app/http/routes.php , 框架的入口文件放在public中
+
+2、框架的模版文件放在：resources/views/yourm/yourm.blade.php  ， 模版文件必须以.blade.php结尾
+
+3、客户端通过表单发送post请求到服务端时，需要在客户端的表单处增加隐藏域 ， get请求不需要
+ 客户端：
+ <form action="/insert" method="post">
+      用户名: <input type="text" name="username" palceholder="please input" />
+      密码： <input type="password" name="password" placeholder="password" />
+      {{csrf_field()}}
+ </form>
+
+4、利用跨站请求伪造方式，发送put 或者 delete 请求
+
+ 客户端
+ put
+ <form action="/update" method="post">
+      <input type="hidden" name="_method" value="PUT" />
+      用户名：<input type="text" name="username" placeholder="username" />
+      密码：  <input type="password" name="password" placeholder="password" />
+      {{ csrf_field() }}
+</form>
+
+delete
+<form action="/update" method="post">
+      <input type="hidden" name="_method" value="PUT" />
+      用户名：<input type="text" name="username" placeholder="username" />
+      密码：  <input type="password" name="password" placeholder="password" />
+      {{ csrf_field() }}
+</form>
+
+服务端路由
+
+Route::put("/update" , function(){
+        echo "update" ;
+});
+
+Route::delete("/delete" , function(){
+        echo "delete" ;
+});
+
+5、带参数的路由匹配规则
+
+服务端：
+Route::get("/article/{id}" , function($id){
+        echo "当前文章的id为" . $id ;
+});
+
+客户端访问：localhost/article/32
+
+6、限制参数类型
+服务端：
+Route::get("/goods/{id}" , function($id){
+        echo "当前文章的id为" . $id ;
+}).where("id" , "\d+") ;
+
+7、传递多个参数
+服务端：
+Route::get("/{type}-{id}" , function($type , $id){
+        echo "当前类型为：" . $type . " ; 当前的id为：" . $id ;
+}) ;
+
+客户端：
+localhost/goods-15
+
+8、路由别名 -- 便于创建复杂、多级路由对应的模版
+
+// route是一个函数，用于快速创建完整url
+Route::get("/admin/users/list" , [
+  "as" => "ulist" ,
+  "uses" => function(){
+    echo route('ulist');
+  }
+]);
+
+9、路由组 -- 可以为某些路由增加中间件，用于组别路由的验证和处理规则
+
+
+Route::group([] , function(){
+    Route::get("/{type}-{id}" , function($type , $id){
+        echo "当前类型为：" . $type . " ; 当前的id为：" . $id ;
+    }) ;
+
+    Route::get("/goods/{id}" , function($id){
+        echo "当前文章的id为" . $id ;
+    }).where("id" , "\d+") ;
+});
+
+
+10、关于路由404的设置，当路由规则匹配不到规则的时候，就会显示这个404页面内容
+在resorces/view/error新建404.blade.php文件 ， 可以作为404错误信息页面
+服务端：
+Route::get("/404" , function(){
+  echo view("404") ;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
