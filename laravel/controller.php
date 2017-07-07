15d2cdadb3a06c320d28109a6b5808cd8cc8f404
liuding <liuding@ifmuse.com>
2017-07-07 11:55:19 +0800

    update controller

--- laravel/controller.php
+++ laravel/controller.php
@@ -34,8 +34,49 @@ Route::get("/user/shengji/{id}" , [
 	"middleware" => "login" ,
 	"uses" => 'TestController@shengji'
 ]) ;
-
+// 方式二
 Route::get("/user/jinyong/{id}" , 'TestController@jinyong')->middleware('login') ;
+
+5、隐式控制器
+
+1> 创建GoodsController控制器
+php artisan make:controller GoodsController --plain
+
+2> 修改GoodsController.php代码
+
+3> 修改routes.php，新建路由匹配规则
+// 表示路由中以goods开头的所有请求，都会交给GoodsController处理，
+// 例如：localhost/goods/add , localhost/goods/del
+Route::controller("goods" , "GoodsController") ;
+
+注意：要在对应的控制器方法名前面加上请求的方式(加粗)
+例如：使用get方式请求goods/add的方式，那么控制器中对应的方法应该是用驼峰的方式写成getAdd：
+class GoodsController extends Controller {
+	public function getAdd(){
+		return view("add") ;
+	}
+}
+
+4> 新建add.blade.php模版文件，输入：
+<form action="/goods/add" method="post">
+	用户名： <input type="text" name="username" palceholder="username" />
+	</br>
+	{{csrf_filed()}}
+	<input type="submit" value="submit" />
+</from>
+
+
+6、Restful控制器 -- 匹配固定7种方式的路由规则
+
+1> 创建PhotosController
+php artisan make:controller PhotosController
+
+2> 修改routes.php，新建路由匹配规则
+
+// 通过Route::resource()建立Restful控制器
+Route::resource("photos" , "PhotosController") ;
+
+3> 修改PhotosController.php文件
 
 
 */
@@ -65,4 +106,87 @@ Route::get("/user/jinyong/{id}" , 'TestController@jinyong')->middleware('login')
 		}
 	}
 ?>
+
+<?php
+	// GoodsController.php
+	namespace App\Http\Controller ;
+	use Illuminate\Http\Request ;
+	use App\Http\Request ;
+	use App\Http\Controllers\Controller ;
+	class GoodsController extends Controller
+	{	
+		// 商品添加 -- 传递视图
+		public function getAdd(){
+			// 在laravel中，可以使用return的方式，向客户端返回内容
+			return view("add") ;
+		}
+
+		// 商品添加 -- 传递参数
+		public function postAdd(){
+			return "This is a postAdd page!" ;
+		}
+	}
+?>
+
+
+
+<?php
+	
+	// PhotosController.php
+	namespace App\Http\Controller ;
+	use Illuminate\Http\Request ;
+	use App\Http\Request ;
+	use App\Http\Controllers\Controller ;
+	class PhotosController extends Controller {
+
+		// get /photos http/1.1
+		public function index(){
+
+		}
+
+		// get /photos/create http/1.1
+		public function create(){
+
+		}
+
+		// post /photos http/1.1
+		public function store(){
+
+		}
+
+		// get /photos/{id} http/1.1
+		public function show($id){
+
+		}
+
+		//get /photos/{id}/edit http/1.1
+		public function edit($id){
+
+		}
+
+		// put/patch /photos/{id} http/1.1
+		public function update($id){
+
+		}
+
+		// delete /photos/{id} http/1.1
+		public function destory($id){
+
+		}
+
+	}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
 
