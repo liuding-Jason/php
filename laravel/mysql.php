2464e286a7691c38cb729752c64e2c7dd6430d44
liuding <liuding@ifmuse.com>
2017-07-10 19:48:20 +0800

    update mysql

--- laravel/mysql.php
+++ laravel/mysql.php
@@ -34,7 +34,7 @@ Route::get("/db/delete" , 'DbTestController@delete') ;
 Route::get("/db/create" , 'DbTestController@create') ;
 
 2、事务操作
-事务操作是对一组sql语句进行执行的操作，可以根据每一条语句的执行结果进行判断是否执行回滚操作，回滚成功之后，所有这个组的语句都将不执行。
+事务操作是对一组sql语句进行执行的操作，可以根据每一条语句的执行结果进行判断是否执行回滚操作，回滚成功之后，所有这个组的语句都将不执行。保证数据的完整性和安全性。
 
 常用语句：
 开启事务：Db::beginTransaction()
@@ -44,14 +44,33 @@ Route::get("/db/create" , 'DbTestController@create') ;
 使用方法：
 1> 修改routes.php文件，新建路由匹配规则：
 Route::get("/db/affair" , 'DbTestController@affair');
-2>
+
+3、在主从数据库之间实现读写分离
+
+1> 进入laravel根路径下，找到config/database.php，新建从数据库的配置：
+"slever-1" : [
+	'dirver' 	=> 'mysql' ,
+	'host'		=> '192.168.60.54' ,
+	'database'  => 'test' ,
+	'username'  => 'root' ,
+	'password'  => '' ,
+	'charset'   => 'utf-8' ,
+	'collation' => 'utf8_unicode_ci' ,
+	'prefix'	=> '' ,
+	'strict'	=> false
+]
+
+2> 修改routes.php，新增路由匹配规则：
+Route::get("/db/slever" , 'DbTestController@sleverTest');
+
+
+
 
  */
 
 ?>
 
 <?php
-	
 	// DbTestController.php
 	namespace App\Http\Controller ;
 	use DB ;
@@ -86,10 +105,8 @@ Route::get("/db/affair" , 'DbTestController@affair');
  			Db::beginTransaction() ;
  			// 扣钱操作
  			$setRes = Db::update('update acount set acount=acount - 2000 where id = 18') ;
- 			
  			// 加钱操作
  			$delRes = Db::update('update ourAcount set acount = acount + 2000 where id = 18') ;
- 			
  			if($setRes&&$delRes){
  				Db::commit() ;
  				echo "转账成功！" ;
@@ -97,9 +114,10 @@ Route::get("/db/affair" , 'DbTestController@affair');
  				DB::rollback() ;
  				echo "转账失败！";
  			}
-
  		}
-
+ 		// 执行从数据库的操作
+ 		public function sleverTest(){
+ 			$res = DB::connection("slever").select() ; // update insert delete执行相关的操作
+ 		}
 	}
-
 ?>
